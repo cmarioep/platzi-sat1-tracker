861f34dcf65ebacbe974d8c638d9ab6a77d097fe
Jose Tellez <jdrgv999@gmail.com>
2023-07-31 16:02:51 -0500

    Predicting the position with a given date logic is completed

---
+++ component/Viewer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Viewer () {
+
+   return (
+    <>
+        <div>
+            <h1>Viewer</h1>
+        </div>
+    </>
+   )
+    
+}
+
+export default Viewer;
