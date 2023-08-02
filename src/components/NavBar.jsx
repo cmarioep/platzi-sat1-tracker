5a830b6bf86cc29af4e861a7506228e2b72cd042
cmarioep <cmescandonpardo@gmail.com>
2023-08-01 20:46:45 -0500

    Feature: items and logo at NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,18 @@
-import {Logo} from './Logo';
+import { Logo } from './Logo';
 
 export const NavBar = () => {
 
     return (
         <nav className="navbar">
-            <Logo/>
+
+            <Logo />
+
+            <ul className='navbar__features'>
+                <li className='navbar__features__item'>Enfocar</li>
+                <li className='navbar__features__item'>Rastrear</li>
+                <li className='navbar__features__item'>Mi estación</li>
+            </ul>
+
         </nav>
     )
 
