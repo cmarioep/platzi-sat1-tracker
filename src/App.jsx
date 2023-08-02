f9c81123fcc0f3f3e6662c42541dced8e4e755ce
cmarioep <cmescandonpardo@gmail.com>
2023-08-01 20:34:18 -0500

    Feature: NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { MapView } from './components/MapView'
+import { MapView } from './components/MapView';
+import { NavBar } from './components/NavBar';
+
 
 function App() {
 
 
   return (
-    <>
+    <div className='layout'>
+      <NavBar />
       <MapView />
-    </>
+    </div>
   )
 }
 
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import {Logo} from './Logo';
+
+export const NavBar = () => {
+
+    return (
+        <nav className="navbar">
+            <Logo/>
+        </nav>
+    )
+
+}
