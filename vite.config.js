51f432aa2a7701d926b77a1455b5ba8d70397726
cmarioep <cmarioescandonpardo@gmail.com>
2023-08-02 11:40:26 -0500

    Feature: basci config for PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from "vite-plugin-pwa";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA(
+      {
+        registerType: 'autoUpdate',
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,png,webp}'],
+        },
+        manifest: {
+          name: 'Satellite Tracker',
+          short_name: 'Platzi-Sat1-Tracker',
+          description: 'Satellite Tracker',
+          background_color: '#090909',
+          theme_color: '#090909',
+          icons: [
+            {
+              src: '/Icon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: '/Icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png'
+            },
+            {
+              src: '/Icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable'
+            },
+
+          ],
+          handler: "CacheFirst",
+          devOptions: {
+            enabled: true
+          }
+        }
+      }
+    )
+  ],
 })
