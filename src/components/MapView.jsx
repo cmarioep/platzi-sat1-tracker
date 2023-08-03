aa21e99b659d8bfd7cb7ec86a5a08a180c1faec0
cmarioep <cmescandonpardo@gmail.com>
2023-08-02 22:00:08 -0500

    Feature: Using custom hook to get satellite position and set a marker on map

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,41 +1,67 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import useSatellite from '../../hooks/useSatellite';
 
 import "leaflet/dist/leaflet.css";
 import '../styles/components/MapView.scss';
 
-
 export const MapView = () => {
+    const [mapCenter, setMapCenter] = useState([0, 0]);
+    const [satellitePosition, setSatellitePosition] = useState(null);
+    const [userPosition, setUserPosition] = useState(null);
+    const { getSatRec, getSatelliteInfo, convertECItoLatLong, getUserLocation } = useSatellite();
 
-    const [mapCenter, setMapCenter] = React.useState([0, 0]); // Coordenadas para centrar el mapa inicialmente
+    // Effect to get the satellite position
+    useEffect(() => {
+        const satRec = getSatRec();
+        const currentDate = new Date();
+        const positionAndVelocity = getSatelliteInfo(satRec, currentDate);
+        const position = convertECItoLatLong(positionAndVelocity.position);
+        setSatellitePosition(position);
+    }, [getSatRec, getSatelliteInfo, convertECItoLatLong]);
 
-    const {
-        getUserLocation
-    } = useSatellite();
-
-    React.useEffect(() => {
-
+    // Effect to get the user position
+    useEffect(() => {
         const userLocationPromise = getUserLocation();
+        userLocationPromise
+            .then((userLocation) => {
+                setUserPosition(userLocation);
+            })
+            .catch((error) => {
+                console.error('Error getting user location:', error.message);
+            });
+    }, [getUserLocation]);
 
-        userLocationPromise.then((userLocation) => {
-            setMapCenter([userLocation.latitude, userLocation.longitude]);
-        });
-
-    }, [mapCenter]);
+    useEffect(() => {
+        if (satellitePosition) {
+            setMapCenter([satellitePosition.latitude, satellitePosition.longitude]);
+        }
+    }, [satellitePosition]);
 
     return (
-        <MapContainer center={mapCenter} zoom={3} scrollWheelZoom={false}>
+        <MapContainer center={mapCenter} zoom={2} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
                 url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
             />
-            <Marker position={mapCenter}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            {satellitePosition && (
+                <Marker position={[satellitePosition.latitude, satellitePosition.longitude]}>
+                    <Popup>
+                        Satellite Position. <br />
+                        Latitude: {satellitePosition.latitude} <br />
+                        Longitude: {satellitePosition.longitude}
+                    </Popup>
+                </Marker>
+            )}
+            {userPosition && (
+                <Marker position={[userPosition.latitude, userPosition.longitude]}>
+                    <Popup>
+                        Your Position. <br />
+                        Latitude: {userPosition.latitude} <br />
+                        Longitude: {userPosition.longitude}
+                    </Popup>
+                </Marker>
+            )}
         </MapContainer>
     );
-
-}
+};
