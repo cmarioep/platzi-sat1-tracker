a27568291d2e99ee1fc62805a701f731cdc8eaed
Jose Tellez <jdrgv999@gmail.com>
2023-08-05 15:50:10 -0500

    mergin with remote

--- src/hooks/useSatellite/index.js
+++ src/hooks/useSatellite/index.js
@@ -4,6 +4,9 @@ import { twoline2satrec, propagate } from 'satellite.js';
 const platziSatTLE1 = '1 88888U 24001FA  23163.94096086  .00000000  00000-0  10000-4 0  9999';
 const platziSatTLE2 = '2 88888  97.5077 280.5424 0008220 228.6198 130.8530 15.11803180  1009'
 
+const uno = "1 88888U 24001FA  23163.94096086  .00000000  00000-0  10000-4 0  9999"
+const dos = "2 88888  97.5077 280.5424 0008220 228.6198 130.8530 15.11803180  1009"
+
 export default function useSatellite() {
 
     /**
